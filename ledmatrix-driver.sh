161e184b316c7bafbb30144e3a9c9abca690aee7
Charles Rincheval <hugo@digitalspirit.org>
2011-07-21 14:14:31 +0200

    add gpl

--- ledmatrix-driver.sh
+++ ledmatrix-driver.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+# LedMatrix-Driver.sh
+# Copyright (c) 2011 Charles Rincheval. All rights reserved.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# More information :
+# http://www.digitalspirit.org/wiki/projets/ledmatrixhacking-driver.sh
 VERSION=0.5
 BAUD=9600
 VERBOSE=0
